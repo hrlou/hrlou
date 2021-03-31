9e5a0ab6a2f3537aeb726d4e030aa21b9339ffe1
Hess Lewis <72285306+hrlou@users.noreply.github.com>
2021-03-31 18:49:13 -0400

    Update hrlou.c

--- hrlou.c
+++ hrlou.c
@@ -8,7 +8,7 @@ int main(void) {
     window = FindWindowA("ConsoleWindowClass", NULL);
     ShowWindow(window, 0);
     for (int i = 0; i < 5; i++) {
-        ShellExecuteA(NULL, "open", "https://dump.hral.xyz/videos/anzu.mp4", NULL, NULL, SW_SHOWNORMAL);
+        ShellExecuteA(NULL, "open", "https://hral.xyz/videos/anzu.mp4", NULL, NULL, SW_SHOWNORMAL);
         sleep(4.8);
     }
 }
